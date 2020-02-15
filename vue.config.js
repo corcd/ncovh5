fe6e9ab428d6225d9f2eb8d1aaa5bdd36fa18b9b
王含泽 <whzcorcd@gmail.com>
2020-02-15 22:20:43 +0800

    [FE] (update) 1. 基于 webpack4 完成拆包优化

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,24 @@ module.exports = {
       args[0].terserOptions.compress.drop_console = true
       return args
     })
+    config.optimization.splitChunks({
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 200000,
+      automaticNameDelimiter: '-',
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1]
+            return `chunk.${packageName.replace('@', '')}`
+          },
+          priority: 10
+        }
+      }
+    })
   },
   transpileDependencies: ['vue-echarts', 'resize-detector']
 }
