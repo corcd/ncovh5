f155996608ed0bf9e5976afd5576ecbb74162106
王含泽 <whzcorcd@gmail.com>
2020-02-15 17:32:07 +0800

    [FE] (update) 1. 基于 webpack4 完成拆包优化

--- src/utils/index.js
+++ src/utils/index.js
@@ -16,7 +16,7 @@ export default {
   },
   isWx: () => {
     const ua = window.navigator.userAgent.toLowerCase()
-    console.log(ua) //mozilla/5.0 (iphone; cpu iphone os 9_1 like mac os x) applewebkit/601.1.46 (khtml, like gecko)version/9.0 mobile/13b143 safari/601.1
+    // console.log(ua) //mozilla/5.0 (iphone; cpu iphone os 9_1 like mac os x) applewebkit/601.1.46 (khtml, like gecko)version/9.0 mobile/13b143 safari/601.1
     if (ua.match(/MicroMessenger/i) == 'micromessenger') {
       return true
     } else {
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,28 @@
 module.exports = {
   publicPath: '/ncovh5/',
-  productionSourceMap: true,
+  productionSourceMap: false,
   chainWebpack: config => {
     config.optimization.minimizer('terser').tap(args => {
       args[0].terserOptions.compress.drop_console = true
       return args
+    })
+    config.optimization.splitChunks({
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 300000, // 依赖包超过300000bit将被单独打包
+      automaticNameDelimiter: '-',
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1]
+            return `chunk.${packageName.replace('@', '')}`
+          },
+          priority: 10
+        }
+      }
     })
   },
   transpileDependencies: ['vue-echarts', 'resize-detector']
