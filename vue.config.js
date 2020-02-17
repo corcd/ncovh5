3563e3b879854acbf9c7946f05313d36a47ed37e
王含泽 <whzcorcd@gmail.com>
2020-02-17 21:59:38 +0800

    [FE] (update) 1. 优化图片大小和 css 加载

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   publicPath: '/ncovh5/',
-  productionSourceMap: true,
+  productionSourceMap: false,
+  css: {
+    extract: false,
+    sourceMap: false
+  },
   chainWebpack: config => {
     config.optimization.minimizer('terser').tap(args => {
       args[0].terserOptions.compress.drop_console = true
